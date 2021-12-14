55df44238804e2b57ff149695205ce38994f3c22
shivaprasad <shivaprasad9490@gmail.com>
2021-12-14 14:32:26 +0530

    first commit

---
+++ kaya.java
@@ -0,0 +1,2 @@
+kaya is the goood boy he is from uk and he knows lot of information about sap abap and java related information 
+amazon is the tp com
---
+++ maya.java
@@ -0,0 +1,1 @@
+maya is the good girl and she studed in human resource subject
